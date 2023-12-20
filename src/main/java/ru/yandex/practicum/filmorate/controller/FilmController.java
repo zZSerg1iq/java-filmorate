8bb61a95b0f63001848a82298a94788911f28200
ZzSerg <azuraka@mail.ru>
2023-12-20 20:47:29 +0500

    feat: подготовлены контроллеры

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,73 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.servise.FilmDataStorageService;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
+
+    private final FilmDataStorageService filmStorageService;
+
+    @Autowired
+    public FilmController(FilmDataStorageService filmStorageService) {
+        this.filmStorageService = filmStorageService;
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<String> handleValidationException(ValidationException ex) {
+        log.warn(ex.getMessage());
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Film> getFilm(@PathVariable long id) {
+        var film = filmStorageService.getFilm(id);
+        if (film.getId() != -1) {
+            return ResponseEntity.ok(film);
+        }
+        return new ResponseEntity<>(film, HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Film>> getFilmList() {
+        var filmList = filmStorageService.getFilmList();
+        return ResponseEntity.ok(filmList);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Film> addFilm(@Valid @RequestBody Film film) {
+        var addedFilm = filmStorageService.addFilm(film);
+
+        if (addedFilm.getId() != -1) {
+           log.info("Film added: "+film);
+           return new ResponseEntity<>(film, HttpStatus.CREATED);
+        }
+        log.info("Add film conflict: "+film);
+        return new ResponseEntity<>(film, HttpStatus.CONFLICT);
+    }
+
+    @PutMapping()
+    public ResponseEntity<Film> updateFilm(@Valid @RequestBody Film film) {
+        var updateResult = filmStorageService.updateFilm(film);
+        log.info("Film updated: "+film);
+        return ResponseEntity.ok(updateResult);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFilm(@PathVariable long id) {
+        filmStorageService.deleteFilm(id);
+    }
+
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,66 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.servise.UserDataStorageService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+
+    private final UserDataStorageService userStorageService;
+
+    @Autowired
+    public UserController(UserDataStorageService userStorageService) {
+        this.userStorageService = userStorageService;
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<String> handleValidationException(ValidationException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable long id) {
+        var user = userStorageService.getUser(id);
+        if (user.getId() != -1) {
+            return ResponseEntity.ok(user);
+        }
+        return new ResponseEntity<>(user, HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getUserList() {
+        var userList = userStorageService.getUserList();
+        return ResponseEntity.ok(userList);
+    }
+
+    @PostMapping()
+    public ResponseEntity<User> addUser(@Valid @RequestBody User user) {
+        var addedUser = userStorageService.addUser(user);
+        return new ResponseEntity<>(addedUser, HttpStatus.CREATED);
+    }
+
+    @PutMapping()
+    public ResponseEntity<User> updateUser(@Valid @RequestBody User user) {
+        var updateResult = userStorageService.updateUser(user);
+        return ResponseEntity.ok(updateResult);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable long id) {
+        userStorageService.deleteUser(id);
+    }
+
+
+}
