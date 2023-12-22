811243d5637e628b3a39fff55e14779837e3a1d3
ZzSerg <azuraka@mail.ru>
2023-12-22 16:35:55 +0500

    new test2

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,19 +1,19 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.List;
 
 
 class FilmControllerTest {
-
-    /**
-     * Все тесты работают. Запускались отдельно от сервиса. То есть запускался сначала FilmorateApplication
-     * а потом на работающий веб-сервис запускались тесты которые тестили контроллеры, валидацию итд
-     * Точно так же тестировалось все через постман
-     * ГИТхаб тесты пройти не получается, постоянно ошибки пишет. Пришлось закоментировать тесты.
-     * Пишу это на случай, если гит пропустит "пустые тесты"
-     */
-
-
 
     //Тест на гитхабе заставил переписать имя константы в нижнем регистре), потому что:
     //Error:  /home/runner/work/java-filmorate/java-filmorate/src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java:19:26:
@@ -22,45 +22,44 @@ class FilmControllerTest {
 
     @Test
     void getFilm() {
-        /*
         LocalDate date = LocalDate.of(2002, 2, 2);
         Film film = new Film(0, "Человек дождя", "Какое-то описание", date, 10);
         Assertions.assertEquals(HttpStatus.CREATED, sendRequest(HttpMethod.POST, film));
 
         //получаю список у сервиса
         List<Film> returned = sendGetList();
-        assertNotNull(returned);
+        Assertions.assertNotNull(returned);
 
         // получаю фильм из списка
         Film filmFromList = returned.stream().filter(film1 -> film1.getName().equals("Человек дождя")).findFirst().orElse(null);
-        assertNotNull(filmFromList);
+        Assertions.assertNotNull(filmFromList);
 
         //получаю фильм по id у сервиса
         Film film1 = sendGetOne(filmFromList.getId());
-        assertNotNull(film1);
-        assertEquals(film1, filmFromList);
+        Assertions.assertNotNull(film1);
+        Assertions.assertEquals(film1, filmFromList);
 
         // пробую получить несуществующий фильм
         Executable executable = () -> sendGetOne(3244);
-        assertThrows(HttpClientErrorException.NotFound.class, executable);
-        */
+        Assertions.assertThrows(HttpClientErrorException.NotFound.class, executable);
+
     }
 
 
     @Test
     void getFilmList() {
-/*        LocalDate date = LocalDate.of(2003, 2, 2);
+        LocalDate date = LocalDate.of(2003, 2, 2);
         Film film = new Film(0, "ListList", "ListList", date, 10);
         Assertions.assertEquals(HttpStatus.CREATED, sendRequest(HttpMethod.POST, film));
 
         List<Film> returned = sendGetList();
-        assertNotNull(returned);
-        assert returned.size() >= 1;*/
+        Assertions.assertNotNull(returned);
+        assert returned.size() >= 1;
     }
 
     @Test
     void addFilm() {
-        /*LocalDate date = LocalDate.of(1899, 2, 2);
+        LocalDate date = LocalDate.of(1899, 2, 2);
         Film film = new Film(0, "name", "desc", date, 10);
 
         // добавляю корректный фильм
@@ -69,26 +68,26 @@ class FilmControllerTest {
 
         // добавляю копию фильма
         Executable executable = () -> sendRequest(HttpMethod.POST, film);
-        assertThrows(HttpClientErrorException.Conflict.class, executable);
+        Assertions.assertThrows(HttpClientErrorException.Conflict.class, executable);
 
 
         // фильм с null полем даты
         Film filmLocalDateIsNull = new Film(0, "name", "desc", null, 10);
         Executable executable2 = () -> sendRequest(HttpMethod.POST, filmLocalDateIsNull);
-        assertThrows(HttpClientErrorException.BadRequest.class, executable2);
+        Assertions.assertThrows(HttpClientErrorException.BadRequest.class, executable2);
 
         // фильм с полем даты ниже минимальной границы
         date = LocalDate.of(1699, 2, 2);
         Film wrongFilmLocalDate = new Film(0, "name", "desc", date, 10);
         Executable executableT = () -> sendRequest(HttpMethod.POST, wrongFilmLocalDate);
-        assertThrows(HttpClientErrorException.BadRequest.class, executableT);
+        Assertions.assertThrows(HttpClientErrorException.BadRequest.class, executableT);
 
 
         //пустое название фильма
         date = LocalDate.of(1999, 2, 2);
         Film wrongName = new Film(0, null, "description", date, 10);
         Executable executable3 = () -> sendRequest(HttpMethod.POST, wrongName);
-        assertThrows(HttpClientErrorException.BadRequest.class, executable3);
+        Assertions.assertThrows(HttpClientErrorException.BadRequest.class, executable3);
 
 
         // слишком длинное описание
@@ -96,45 +95,45 @@ class FilmControllerTest {
         date = LocalDate.of(2006, 2, 2);
         Film tooLongDescription = new Film(0, "name", description, date, 10);
         Executable executable4 = () -> sendRequest(HttpMethod.POST, tooLongDescription);
-        assertThrows(HttpClientErrorException.BadRequest.class, executable4);
+        Assertions.assertThrows(HttpClientErrorException.BadRequest.class, executable4);
 
         // пустое описание
         description = "";
         date = LocalDate.of(2006, 4, 2);
         Film noDescription = new Film(0, "name", description, date, 10);
         Executable executableYo = () -> sendRequest(HttpMethod.POST, noDescription);
-        assertDoesNotThrow(executableYo);
+        Assertions.assertDoesNotThrow(executableYo);
 
 
         // продолжительность 0 секунд
         date = LocalDate.of(2007, 2, 2);
         Film wrongDuration = new Film(0, "the name", "description", date, 0);
         Executable executable5 = () -> sendRequest(HttpMethod.POST, wrongDuration);
-        assertThrows(HttpClientErrorException.BadRequest.class, executable5);
+        Assertions.assertThrows(HttpClientErrorException.BadRequest.class, executable5);
 
         // продолжительность отрицательная
         date = LocalDate.of(2007, 6, 2);
         Film wrongDuration2 = new Film(0, "the name", "description", date, -1);
         Executable executable6 = () -> sendRequest(HttpMethod.POST, wrongDuration2);
-        assertThrows(HttpClientErrorException.BadRequest.class, executable6);
+        Assertions.assertThrows(HttpClientErrorException.BadRequest.class, executable6);
 
         // неотрицательная продолжительность
         date = LocalDate.of(2009, 6, 2);
         Film wrongDuration3 = new Film(0, "the name", "description", date, 1);
         Executable executable7 = () -> sendRequest(HttpMethod.POST, wrongDuration3);
-        assertDoesNotThrow(executable7);*/
+        Assertions.assertDoesNotThrow(executable7);
     }
 
     @Test
     void updateFilm() {
-       /* // добавление фильма для последующего изменения (в случае, если тест будет запущен первым)
+        // добавление фильма для последующего изменения (в случае, если тест будет запущен первым)
         LocalDate date = LocalDate.of(2001, 2, 2);
         Film film = new Film(0, "name", "desc", date, 10);
         Assertions.assertEquals(HttpStatus.CREATED, sendRequest(HttpMethod.POST, film));
 
         // получение списка и фильма из него
         List<Film> returned = sendGetList();
-        assertNotNull(returned);
+        Assertions.assertNotNull(returned);
         Film returnedFilm = returned.get(0);
         long id = returnedFilm.getId();
 
@@ -145,15 +144,14 @@ class FilmControllerTest {
 
         // получение отредактированного фильма и сравнение
         Film returnedFilm2 = sendGetOne(id);
-        assertEquals("New name lalala", returnedFilm2.getName());
-        assertEquals("New description lalala", returnedFilm2.getDescription());
-        assertEquals(returnedFilm2, returnedFilm);*/
+        Assertions.assertEquals("New name lalala", returnedFilm2.getName());
+        Assertions.assertEquals("New description lalala", returnedFilm2.getDescription());
+        Assertions.assertEquals(returnedFilm2, returnedFilm);
     }
 
     @Test
     void deleteFilm() {
     }
-/*
 
     private HttpStatus sendRequest(HttpMethod method, Film film) {
         HttpHeaders headers = new HttpHeaders();
@@ -185,7 +183,7 @@ class FilmControllerTest {
 
         return response.getBody();
     }
-*/
+
 
 
 }
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -1,17 +1,21 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.List;
 
 
 class UserControllerTest {
-
-    /**
-     * Все тесты работают. Запускались отдельно от сервиса. То есть запускался сначала FilmorateApplication
-     * а потом на работающий веб-сервис запускались тесты которые тестили контроллеры, валидацию итд
-     * Точно так же тестировалось все через постман
-     * ГИТхаб тесты пройти не получается, постоянно ошибки пишет. Пришлось закоментировать тесты.
-     * Пишу это на случай, если гит пропустит "пустые тесты"
-     */
 
     //Тест на гитхабе заставил переписать имя константы в нижнем регистре), потому что:
     //Error:  /home/runner/work/java-filmorate/java-filmorate/src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java:19:26:
@@ -20,44 +24,44 @@ class UserControllerTest {
 
     @Test
     void getUser() {
-        /*LocalDate date = LocalDate.of(2000, 2, 2);
+        LocalDate date = LocalDate.of(2000, 2, 2);
         User user = new User(0, "Аквамен", "aquamenBoYYYYY", "mail@mail.mail", date);
         Assertions.assertEquals(HttpStatus.CREATED, sendRequest(HttpMethod.POST, user));
 
         //получаю список у сервиса
         List<User> returned = sendGetList();
-        assertNotNull(returned);
+        Assertions.assertNotNull(returned);
 
         // получаю пользователя из списка
         User userFromList = returned.stream()
                 .filter(u -> u.getName().equals("Аквамен"))
                 .findFirst().orElse(null);
-        assertNotNull(userFromList);
+        Assertions.assertNotNull(userFromList);
 
         //получаю пользователя по id у сервиса
         User user1 = sendGetOne(userFromList.getId());
-        assertNotNull(user1);
-        assertEquals(user1, userFromList);
+        Assertions.assertNotNull(user1);
+        Assertions.assertEquals(user1, userFromList);
 
         // пробую получить несуществующего пользователя
         Executable executable = () -> sendGetOne(3244);
-        assertThrows(HttpClientErrorException.NotFound.class, executable);*/
+        Assertions.assertThrows(HttpClientErrorException.NotFound.class, executable);
     }
 
     @Test
     void getUserList() {
-        /*LocalDate date = LocalDate.of(1995, 2, 2);
+        LocalDate date = LocalDate.of(1995, 2, 2);
         User user = new User(0, "UserUser", "UserLogin", "Email@email.email", date);
         Assertions.assertEquals(HttpStatus.CREATED, sendRequest(HttpMethod.POST, user));
 
         List<User> returned = sendGetList();
-        assertNotNull(returned);
-        assert returned.size() >= 1;*/
+        Assertions.assertNotNull(returned);
+        assert returned.size() >= 1;
     }
 
     @Test
     void addUser() {
-        /*// создаю и добавляю пользователя
+        // создаю и добавляю пользователя
         LocalDate date = LocalDate.of(1999, 2, 2);
         User user = new User(0, "UserUser", "UserLogin", "Email@email.email", date);
         Assertions.assertEquals(HttpStatus.CREATED, sendRequest(HttpMethod.POST, user));
@@ -78,56 +82,56 @@ class UserControllerTest {
                 .filter(user1 -> user1.getLogin().equals("NameLogin_1"))
                 .findFirst()
                 .orElse(null);
-        assertNotNull(noNameUserReturned);
-        assertEquals("NameLogin_1", noNameUserReturned.getName());
+        Assertions.assertNotNull(noNameUserReturned);
+        Assertions.assertEquals("NameLogin_1", noNameUserReturned.getName());
 
 
         // пользователь, с пустым логином
         date = LocalDate.of(1981, 2, 2);
         User wrongLoginUser = new User(0, "UserUser", null, "Email@email.email", date);
         Executable executableT = () -> sendRequest(HttpMethod.POST, wrongLoginUser);
-        assertThrows(HttpClientErrorException.BadRequest.class, executableT);
+        Assertions.assertThrows(HttpClientErrorException.BadRequest.class, executableT);
         User wrongLoginUserT = new User(0, "UserUser", "", "Email@email.email", date);
         Executable executableTt = () -> sendRequest(HttpMethod.POST, wrongLoginUserT);
-        assertThrows(HttpClientErrorException.BadRequest.class, executableTt);
+        Assertions.assertThrows(HttpClientErrorException.BadRequest.class, executableTt);
 
 
         // некорректный емейл
         date = LocalDate.of(2099, 2, 2);
         User wrongEmail = new User(0, "UserUser1", "UserLogin1", "Email.email", date);
         Executable executableF = () -> sendRequest(HttpMethod.POST, wrongEmail);
-        assertThrows(HttpClientErrorException.BadRequest.class, executableF);
+        Assertions.assertThrows(HttpClientErrorException.BadRequest.class, executableF);
 
         User wrongEmail2 = new User(0, "UserUser1", "UserLogin1", "Email@", date);
         Executable executableY = () -> sendRequest(HttpMethod.POST, wrongEmail2);
-        assertThrows(HttpClientErrorException.BadRequest.class, executableY);
+        Assertions.assertThrows(HttpClientErrorException.BadRequest.class, executableY);
 
         User wrongEmail3 = new User(0, "UserUser1", "UserLogin1", "@g.r", date);
         Executable executableU = () -> sendRequest(HttpMethod.POST, wrongEmail3);
-        assertThrows(HttpClientErrorException.BadRequest.class, executableU);
+        Assertions.assertThrows(HttpClientErrorException.BadRequest.class, executableU);
 
         User wrongEmail4 = new User(0, "UserUser1", "UserLogin1", "", date);
         Executable executableO = () -> sendRequest(HttpMethod.POST, wrongEmail4);
-        assertThrows(HttpClientErrorException.BadRequest.class, executableO);
+        Assertions.assertThrows(HttpClientErrorException.BadRequest.class, executableO);
 
 
         // некорректная дата рождения
         date = LocalDate.of(2025, 2, 2);
         User incorrectBirth = new User(0, "UserUser", "UserLogin", "Email@email.email", date);
         Executable executable4 = () -> sendRequest(HttpMethod.POST, incorrectBirth);
-        assertThrows(HttpClientErrorException.BadRequest.class, executable4);*/
+        Assertions.assertThrows(HttpClientErrorException.BadRequest.class, executable4);
     }
 
     @Test
     void updateUser() {
-        /*// добавление пользователя для последующего изменения (в случае, если тест будет запущен первым)
+        // добавление пользователя для последующего изменения (в случае, если тест будет запущен первым)
         LocalDate date = LocalDate.of(2001, 7, 2);
         User user = new User(0, "UserUser", "UserLogin", "Email@email.email", date);
         Assertions.assertEquals(HttpStatus.CREATED, sendRequest(HttpMethod.POST, user));
 
         // получение списка и пользователя
         List<User> returned = sendGetList();
-        assertNotNull(returned);
+        Assertions.assertNotNull(returned);
         User returnedUser = returned.get(0);
         long id = returnedUser.getId();
 
@@ -138,14 +142,13 @@ class UserControllerTest {
 
         // получение отредактированного юзера и сравнение
         User returnedUser2 = sendGetOne(id);
-        assertEquals("TestLogin_yeah_booyyyy", returnedUser2.getName());
-        assertEquals("TestLogin_yeah_booyyyy", returnedUser2.getLogin());*/
+        Assertions.assertEquals("TestLogin_yeah_booyyyy", returnedUser2.getName());
+        Assertions.assertEquals("TestLogin_yeah_booyyyy", returnedUser2.getLogin());
     }
 
     @Test
     void deleteUser() {
     }
-/*
 
     private HttpStatus sendRequest(HttpMethod method, User User) {
         HttpHeaders headers = new HttpHeaders();
@@ -190,5 +193,5 @@ class UserControllerTest {
         );
 
         return response.getBody();
-    }*/
+    }
 }
